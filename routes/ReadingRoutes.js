12396066d3115055dd50a616ef3e1073c154f3db
otonye77 <oamietubodie@gmail.com>
2025-11-16 01:35:33 +0100

    change route name

--- routes/ReadingRoutes.js
+++ routes/ReadingRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const readingController = require("../controllers/readingController");
 
-router.post("/createReading", readingController.createReading);
+router.post("/create", readingController.createReading);
 
 module.exports = router;
