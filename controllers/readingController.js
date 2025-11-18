a52db952b80b2dc3a7d4726684e5ed6ea97b7fd2
otonye77 <oamietubodie@gmail.com>
2025-11-18 20:05:41 +0100

    updated database model

--- controllers/readingController.js
+++ controllers/readingController.js
@@ -2,6 +2,7 @@ const readingService = require("../services/readingService");
 
 const createReading = async (req, res, next) => {
   try {
+    console.log("request ", req.body);
     const reading = await readingService.createReading(req.body);
     res.status(201).json(reading);
   } catch (err) {
--- models/readingModel.js
+++ models/readingModel.js
@@ -27,6 +27,42 @@ const Reading = sequelize.define(
       allowNull: false,
       defaultValue: sequelize.Sequelize.literal("CURRENT_TIMESTAMP"),
     },
+    confirmed_reading: {
+      type: DataTypes.DECIMAL(10, 1),
+      allowNull: true,
+    },
+    extracted_reading: {
+      type: DataTypes.DECIMAL(10, 1),
+      allowNull: true,
+    },
+    reading_url: {
+      type: DataTypes.STRING(2048),
+      allowNull: true,
+    },
+    geolocation: {
+      type: sequelize.Sequelize.GEOMETRY("POINT"),
+      allowNull: true,
+    },
+    scheme_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "scheme_master",
+        key: "id",
+      },
+    },
+    correlation_id: {
+      type: DataTypes.CHAR(36),
+      allowNull: true,
+    },
+    person_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "person_master",
+        key: "id",
+      },
+    },
   },
   {
     timestamps: true,
