422854597ea9789ac6daea6046cf22ce75ee4a15
otonye77 <oamietubodie@gmail.com>
2025-11-18 23:06:21 +0100

    changed response

--- controllers/readingController.js
+++ controllers/readingController.js
@@ -4,7 +4,7 @@ const createReading = async (req, res, next) => {
   try {
     console.log("request ", req.body);
     const reading = await readingService.createReading(req.body);
-    res.status(201).json(reading);
+    res.status(201).json({message: "Reading created sucessfully", data: reading});
   } catch (err) {
     next(err);
   }
